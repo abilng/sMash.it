8daa6ae452a8c523f83111d7209b0432fb7e08d9
abilng <abilngeorge@gmail.com>
2013-04-21 12:55:17 +0530

    Added Popup Content

--- server/src/static/js/requests.js
+++ server/src/static/js/requests.js
@@ -40,3 +40,86 @@ function addSideBar (name,json) {
     $("#accordion").append(heading,div);
     $("#accordion").accordion("refresh");
 }
+
+
+function argumentDiv (parent,json_api) {
+    var div = $("<div>");
+    var apiName = json_api.name;
+    for (var i = 0; i < json_api.attributes.length; i++) {
+        attribute=json_api.attributes[i];
+
+        var attributediv = $("<div class='attribute'>");
+        
+        var label = $("<label>").text(attribute["name"]);
+        label.attr('for',apiName +"_" + attribute["name"]+"_input")
+
+
+        var input = $("<input>")
+        input.attr('name',attribute["name"])
+        input.attr('id',apiName +"_" + attribute["name"]+"_input")
+        
+        if(attribute["type"]== null){
+            input.attr('type','text');
+            input.attr('placeholder',attribute["name"]);
+        } else {
+            switch(attribute["type"].toUpperCase()){
+                case "INT":
+                    input.attr('type','number');
+                    input.attr('placeholder',attribute["type"])
+                    break;
+                case "INTEGER":
+                    input.attr('type','number');
+                    input.attr('placeholder',attribute["type"])
+                    break;
+                case "FLOAT":
+                    input.attr('type','number');
+                    input.attr('step','any');
+                    input.attr('placeholder',attribute["type"])
+                    break;
+                case "INT64":
+                    input.attr('type','number');
+                    input.attr('placeholder',attribute["type"])
+                    break;
+                case "BOOLEAN":
+                    input.attr('type','checkbox');
+                    break;
+                case "BOOL":
+                    input.attr('type',"checkbox");
+                    break;
+                default:
+                    input.attr('type','text');
+                    input.attr('placeholder',attribute["type"])
+                    break;
+            }
+        }
+
+        var comment = $("<p class='comment'>").text(attribute["description"]);
+
+
+        if(attribute["read-only"]) {
+            attributediv.addClass("read-only");
+            if(input.attr('type') != 'checkbox') {
+                input.prop('readonly',true)
+            }
+        }
+        if(attribute["write-once"]){
+            attributediv.addClass("write-once");
+        }  
+        if(attribute["required"]) {
+            attributediv.addClass("required");
+            if(input.attr('type') != 'checkbox') {
+                input.prop('required',true)
+            }
+        }
+        if(attribute["queryable"]) {
+            attributediv.addClass("queryable");
+        }
+        if(attribute["guid"]) {
+            attributediv.addClass("quid");
+        }            
+
+        attributediv.append(label,input,comment);
+        div.append(attributediv);
+    };
+    parent.append(div);
+}
